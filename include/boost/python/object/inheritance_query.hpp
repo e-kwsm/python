73a9f81bbd203c94a37734bcfbbff4d92c8420c8
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 18:21:23 +0900

    include/boost/python/object/inheritance_query.hpp include/boost/python/object/instance.hpp include/boost/python/object/iterator.hpp include/boost/python/object/iterator_core.hpp

--- include/boost/python/object/inheritance_query.hpp
+++ include/boost/python/object/inheritance_query.hpp
@@ -3,15 +3,20 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 #ifndef INHERITANCE_QUERY_DWA2003520_HPP
-# define INHERITANCE_QUERY_DWA2003520_HPP
+#define INHERITANCE_QUERY_DWA2003520_HPP
 
-# include <boost/python/type_id.hpp>
+#include <boost/python/type_id.hpp>
 
-namespace boost { namespace python { namespace objects {
+namespace boost {
+namespace python {
+namespace objects {
 
-BOOST_PYTHON_DECL void* find_static_type(void* p, type_info src, type_info dst);
-BOOST_PYTHON_DECL void* find_dynamic_type(void* p, type_info src, type_info dst);
+BOOST_PYTHON_DECL void *find_static_type(void *p, type_info src, type_info dst);
+BOOST_PYTHON_DECL void *find_dynamic_type(void *p, type_info src,
+                                          type_info dst);
 
-}}} // namespace boost::python::object
+} // namespace objects
+} // namespace python
+} // namespace boost
 
 #endif // INHERITANCE_QUERY_DWA2003520_HPP
--- include/boost/python/object/iterator_core.hpp
+++ include/boost/python/object/iterator_core.hpp
@@ -3,15 +3,19 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 #ifndef ITERATOR_CORE_DWA2002512_HPP
-# define ITERATOR_CORE_DWA2002512_HPP
+#define ITERATOR_CORE_DWA2002512_HPP
 
-# include <boost/python/object_fwd.hpp>
+#include <boost/python/object_fwd.hpp>
 
-namespace boost { namespace python { namespace objects {
+namespace boost {
+namespace python {
+namespace objects {
 
-BOOST_PYTHON_DECL object const& identity_function();
+BOOST_PYTHON_DECL object const &identity_function();
 BOOST_PYTHON_DECL void stop_iteration_error();
 
-}}} // namespace boost::python::object
+} // namespace objects
+} // namespace python
+} // namespace boost
 
 #endif // ITERATOR_CORE_DWA2002512_HPP
