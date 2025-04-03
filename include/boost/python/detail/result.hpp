311bcb50ea595ad427564b79cd0dca3e1594b0b5
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 17:52:43 +0900

    include/boost/python/detail/result.hpp include/boost/python/detail/scope.hpp include/boost/python/detail/sfinae.hpp include/boost/python/detail/string_literal.hpp

--- include/boost/python/detail/result.hpp
+++ include/boost/python/detail/result.hpp
@@ -88,47 +88,48 @@ boost::type<typename X::result_type> *result(X const &, short = 0) {
 // For gcc 4.4 compatability, we must include the
 // BOOST_PP_ITERATION_DEPTH test inside an #else clause.
 #else // BOOST_PP_IS_ITERATING
-#if BOOST_PP_ITERATION_DEPTH() == 1 && BOOST_PP_ITERATION_FLAGS() == BOOST_PYTHON_FUNCTION_POINTER
-# if !(BOOST_WORKAROUND(__MWERKS__, > 0x3100)                      \
-        && BOOST_WORKAROUND(__MWERKS__, BOOST_TESTED_AT(0x3201)))
-#  line BOOST_PP_LINE(__LINE__, result.hpp(function pointers))
-# endif
+#if BOOST_PP_ITERATION_DEPTH() == 1 &&                                         \
+    BOOST_PP_ITERATION_FLAGS() == BOOST_PYTHON_FUNCTION_POINTER
+#if !(BOOST_WORKAROUND(__MWERKS__, > 0x3100) &&                                \
+      BOOST_WORKAROUND(__MWERKS__, BOOST_TESTED_AT(0x3201)))
+#line BOOST_PP_LINE(__LINE__, result.hpp(function pointers))
+#endif
 
-# define N BOOST_PP_ITERATION()
+#define N BOOST_PP_ITERATION()
 
 template <class R BOOST_PP_ENUM_TRAILING_PARAMS_Z(1, N, class A)>
-boost::type<R>* result(R (*)(BOOST_PP_ENUM_PARAMS_Z(1, N, A)), int = 0)
-{
-    return 0;
+boost::type<R> *result(R (*)(BOOST_PP_ENUM_PARAMS_Z(1, N, A)), int = 0) {
+  return 0;
 }
 
-# undef N
+#undef N
 
 /* --------------- pointers-to-members --------------- */
-#elif BOOST_PP_ITERATION_DEPTH() == 1 && BOOST_PP_ITERATION_FLAGS() == BOOST_PYTHON_POINTER_TO_MEMBER
+#elif BOOST_PP_ITERATION_DEPTH() == 1 &&                                       \
+    BOOST_PP_ITERATION_FLAGS() == BOOST_PYTHON_POINTER_TO_MEMBER
 // Outer over cv-qualifiers
 
-# define BOOST_PP_ITERATION_PARAMS_2 (3, (0, BOOST_PYTHON_MAX_ARITY, <boost/python/detail/result.hpp>))
-# include BOOST_PP_ITERATE()
+#define BOOST_PP_ITERATION_PARAMS_2                                            \
+  (3, (0, BOOST_PYTHON_MAX_ARITY, <boost/python/detail/result.hpp>))
+#include BOOST_PP_ITERATE()
 
 #elif BOOST_PP_ITERATION_DEPTH() == 2
-# if !(BOOST_WORKAROUND(__MWERKS__, > 0x3100)                      \
-        && BOOST_WORKAROUND(__MWERKS__, BOOST_TESTED_AT(0x3201)))
-#  line BOOST_PP_LINE(__LINE__, result.hpp(pointers-to-members))
-# endif 
+#if !(BOOST_WORKAROUND(__MWERKS__, > 0x3100) &&                                \
+      BOOST_WORKAROUND(__MWERKS__, BOOST_TESTED_AT(0x3201)))
+#line BOOST_PP_LINE(__LINE__, result.hpp(pointers - to - members))
+#endif
 // Inner over arities
 
-# define N BOOST_PP_ITERATION()
-# define Q BOOST_PYTHON_CV_QUALIFIER(BOOST_PP_RELATIVE_ITERATION(1))
+#define N BOOST_PP_ITERATION()
+#define Q BOOST_PYTHON_CV_QUALIFIER(BOOST_PP_RELATIVE_ITERATION(1))
 
 template <class R, class T BOOST_PP_ENUM_TRAILING_PARAMS_Z(1, N, class A)>
-boost::type<R>* result(R (T::*)(BOOST_PP_ENUM_PARAMS_Z(1, N, A)) Q, int = 0)
-{
-    return 0;
+boost::type<R> *result(R (T::*)(BOOST_PP_ENUM_PARAMS_Z(1, N, A)) Q, int = 0) {
+  return 0;
 }
 
-# undef N
-# undef Q
+#undef N
+#undef Q
 
 #endif // BOOST_PP_ITERATION_DEPTH()
 #endif
--- include/boost/python/detail/sfinae.hpp
+++ include/boost/python/detail/sfinae.hpp
@@ -2,12 +2,12 @@
 // Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #ifndef SFINAE_DWA2004723_HPP
-# define SFINAE_DWA2004723_HPP
+#define SFINAE_DWA2004723_HPP
 
-# include <boost/python/detail/prefix.hpp>
+#include <boost/python/detail/prefix.hpp>
 
-# if defined(BOOST_NO_SFINAE) && !defined(BOOST_MSVC)
-#  define BOOST_PYTHON_NO_SFINAE
-# endif
+#if defined(BOOST_NO_SFINAE) && !defined(BOOST_MSVC)
+#define BOOST_PYTHON_NO_SFINAE
+#endif
 
 #endif // SFINAE_DWA2004723_HPP
