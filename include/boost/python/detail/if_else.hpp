142a8ced4e1df8a67135572bbfb44e265a791335
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 17:32:50 +0900

    include/boost/python/detail/if_else.hpp include/boost/python/detail/indirect_traits.hpp

--- include/boost/python/detail/if_else.hpp
+++ include/boost/python/detail/if_else.hpp
@@ -12,70 +12,42 @@ namespace detail {
 
 template <class T> struct elif_selected;
 
-template <class T>
-struct if_selected
-{
-    template <bool b>
-    struct elif : elif_selected<T>
-    {
-    };
+template <class T> struct if_selected {
+  template <bool b> struct elif : elif_selected<T> {};
 
-    template <class U>
-    struct else_
-    {
-        typedef T type;
-    };
+  template <class U> struct else_ {
+    typedef T type;
+  };
 };
 
-template <class T>
-struct elif_selected
-{
-# if !(defined(__MWERKS__) && __MWERKS__ <= 0x2407)
-    template <class U> class then;
-# else
-    template <class U>
-    struct then : if_selected<T>
-    {
-    };
-# endif 
+template <class T> struct elif_selected {
+#if !(defined(__MWERKS__) && __MWERKS__ <= 0x2407)
+  template <class U> class then;
+#else
+  template <class U> struct then : if_selected<T> {};
+#endif
 };
 
-# if !(defined(__MWERKS__) && __MWERKS__ <= 0x2407)
+#if !(defined(__MWERKS__) && __MWERKS__ <= 0x2407)
 template <class T>
 template <class U>
-class elif_selected<T>::then : public if_selected<T>
-{
-};
-# endif 
+class elif_selected<T>::then : public if_selected<T> {};
+#endif
 
-template <bool b> struct if_
-{
-    template <class T>
-    struct then : if_selected<T>
-    {
-    };
+template <bool b> struct if_ {
+  template <class T> struct then : if_selected<T> {};
 };
 
-struct if_unselected
-{
-    template <bool b> struct elif : if_<b>
-    {
-    };
+struct if_unselected {
+  template <bool b> struct elif : if_<b> {};
 
-    template <class U>
-    struct else_
-    {
-        typedef U type;
-    };
+  template <class U> struct else_ {
+    typedef U type;
+  };
 };
 
-template <>
-struct if_<false>
-{
-    template <class T>
-    struct then : if_unselected
-    {
-    };
+template <> struct if_<false> {
+  template <class T> struct then : if_unselected {};
 };
 
 } // namespace detail
