d44b0ebda6b94b9d447b0ecd722a903f4a28e2fe
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 16:58:50 +0900

    src/numpy/ndarray.cpp src/numpy/numpy.cpp src/numpy/scalars.cpp src/numpy/ufunc.cpp

--- src/numpy/scalars.cpp
+++ src/numpy/scalars.cpp
@@ -7,30 +7,29 @@
 #define BOOST_PYTHON_NUMPY_INTERNAL
 #include <boost/python/numpy/internal.hpp>
 
-namespace boost { namespace python {
-namespace converter 
-{
+namespace boost {
+namespace python {
+namespace converter {
 NUMPY_OBJECT_MANAGER_TRAITS_IMPL(PyVoidArrType_Type, numpy::void_)
-} // namespace boost::python::converter
+} // namespace converter
 
-namespace numpy 
-{
+namespace numpy {
 
 void_::void_(Py_ssize_t size)
-  : object(python::detail::new_reference
-      (PyObject_CallFunction((PyObject*)&PyVoidArrType_Type, const_cast<char*>("i"), size)))
-{}
+    : object(python::detail::new_reference(PyObject_CallFunction(
+          (PyObject *)&PyVoidArrType_Type, const_cast<char *>("i"), size))) {}
 
-void_ void_::view(dtype const & dt) const 
-{
-  return void_(python::detail::new_reference
-    (PyObject_CallMethod(this->ptr(), const_cast<char*>("view"), const_cast<char*>("O"), dt.ptr())));
+void_ void_::view(dtype const &dt) const {
+  return void_(python::detail::new_reference(
+      PyObject_CallMethod(this->ptr(), const_cast<char *>("view"),
+                          const_cast<char *>("O"), dt.ptr())));
 }
 
-void_ void_::copy() const 
-{
-  return void_(python::detail::new_reference
-    (PyObject_CallMethod(this->ptr(), const_cast<char*>("copy"), const_cast<char*>(""))));
+void_ void_::copy() const {
+  return void_(python::detail::new_reference(PyObject_CallMethod(
+      this->ptr(), const_cast<char *>("copy"), const_cast<char *>(""))));
 }
 
-}}} // namespace boost::python::numpy
+} // namespace numpy
+} // namespace python
+} // namespace boost
--- src/numpy/ufunc.cpp
+++ src/numpy/ufunc.cpp
@@ -8,58 +8,50 @@
 #include <boost/python/numpy/internal.hpp>
 #include <boost/python/numpy/ufunc.hpp>
 
-namespace boost { namespace python {
-namespace converter 
-{
+namespace boost {
+namespace python {
+namespace converter {
 NUMPY_OBJECT_MANAGER_TRAITS_IMPL(PyArrayMultiIter_Type, numpy::multi_iter)
-} // namespace boost::python::converter
+} // namespace converter
 
-namespace numpy 
-{
+namespace numpy {
 
-multi_iter make_multi_iter(object const & a1)
-{
-  return multi_iter(python::detail::new_reference(PyArray_MultiIterNew(1, a1.ptr())));
+multi_iter make_multi_iter(object const &a1) {
+  return multi_iter(
+      python::detail::new_reference(PyArray_MultiIterNew(1, a1.ptr())));
 }
 
-  multi_iter make_multi_iter(object const & a1, object const & a2)
-{
-  return multi_iter(python::detail::new_reference(PyArray_MultiIterNew(2, a1.ptr(), a2.ptr())));
+multi_iter make_multi_iter(object const &a1, object const &a2) {
+  return multi_iter(python::detail::new_reference(
+      PyArray_MultiIterNew(2, a1.ptr(), a2.ptr())));
 }
 
-multi_iter make_multi_iter(object const & a1, object const & a2, object const & a3)
-{
-  return multi_iter(python::detail::new_reference(PyArray_MultiIterNew(3, a1.ptr(), a2.ptr(), a3.ptr())));
+multi_iter make_multi_iter(object const &a1, object const &a2,
+                           object const &a3) {
+  return multi_iter(python::detail::new_reference(
+      PyArray_MultiIterNew(3, a1.ptr(), a2.ptr(), a3.ptr())));
 }
 
-void multi_iter::next() 
-{
-  PyArray_MultiIter_NEXT(ptr());
+void multi_iter::next() { PyArray_MultiIter_NEXT(ptr()); }
+
+bool multi_iter::not_done() const { return PyArray_MultiIter_NOTDONE(ptr()); }
+
+char *multi_iter::get_data(int i) const {
+  return reinterpret_cast<char *>(PyArray_MultiIter_DATA(ptr(), i));
 }
 
-bool multi_iter::not_done() const 
-{
-  return PyArray_MultiIter_NOTDONE(ptr());
+int multi_iter::get_nd() const {
+  return reinterpret_cast<PyArrayMultiIterObject *>(ptr())->nd;
 }
 
-char * multi_iter::get_data(int i) const 
-{
-  return reinterpret_cast<char*>(PyArray_MultiIter_DATA(ptr(), i));
+Py_intptr_t const *multi_iter::get_shape() const {
+  return reinterpret_cast<PyArrayMultiIterObject *>(ptr())->dimensions;
 }
 
-int multi_iter::get_nd() const
-{
-  return reinterpret_cast<PyArrayMultiIterObject*>(ptr())->nd;
+Py_intptr_t multi_iter::shape(int n) const {
+  return reinterpret_cast<PyArrayMultiIterObject *>(ptr())->dimensions[n];
 }
 
-Py_intptr_t const * multi_iter::get_shape() const 
-{
-  return reinterpret_cast<PyArrayMultiIterObject*>(ptr())->dimensions;
-}
-
-Py_intptr_t multi_iter::shape(int n) const
-{
-  return reinterpret_cast<PyArrayMultiIterObject*>(ptr())->dimensions[n];
-}
-
-}}} // namespace boost::python::numpy
+} // namespace numpy
+} // namespace python
+} // namespace boost
