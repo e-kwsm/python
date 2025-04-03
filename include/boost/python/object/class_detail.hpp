ccdea9c917761e1da14e0886a4ae03622f84bb59
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 18:18:18 +0900

    include/boost/python/object/class_detail.hpp include/boost/python/object/class_metadata.hpp

--- include/boost/python/object/class_detail.hpp
+++ include/boost/python/object/class_detail.hpp
@@ -3,17 +3,21 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 #ifndef CLASS_DETAIL_DWA200295_HPP
-# define CLASS_DETAIL_DWA200295_HPP
+#define CLASS_DETAIL_DWA200295_HPP
 
-# include <boost/python/handle.hpp>
-# include <boost/python/type_id.hpp>
+#include <boost/python/handle.hpp>
+#include <boost/python/type_id.hpp>
 
-namespace boost { namespace python { namespace objects { 
+namespace boost {
+namespace python {
+namespace objects {
 
 BOOST_PYTHON_DECL type_handle registered_class_object(type_info id);
 BOOST_PYTHON_DECL type_handle class_metatype();
 BOOST_PYTHON_DECL type_handle class_type();
 
-}}} // namespace boost::python::object
+} // namespace objects
+} // namespace python
+} // namespace boost
 
 #endif // CLASS_DETAIL_DWA200295_HPP
