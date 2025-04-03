b264683aac3866bdfb7b919c8447ab774d261195
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 18:11:41 +0900

    include/boost/python/raw_function.hpp include/boost/python/refcount.hpp include/boost/python/reference_existing_object.hpp include/boost/python/register_ptr_to_python.hpp

--- include/boost/python/register_ptr_to_python.hpp
+++ include/boost/python/register_ptr_to_python.hpp
@@ -9,23 +9,16 @@
 #include <boost/python/object.hpp>
 #include <boost/python/object/class_wrapper.hpp>
 
-namespace boost { namespace python {
-    
-template <class P>
-void register_ptr_to_python()
-{
-    typedef typename boost::python::pointee<P>::type X;
-    objects::class_value_wrapper<
-        P
-      , objects::make_ptr_instance<
-            X
-          , objects::pointer_holder<P,X>
-        >
-    >();
-}           
+namespace boost {
+namespace python {
 
-}} // namespace boost::python
+template <class P> void register_ptr_to_python() {
+  typedef typename boost::python::pointee<P>::type X;
+  objects::class_value_wrapper<
+      P, objects::make_ptr_instance<X, objects::pointer_holder<P, X>>>();
+}
+
+} // namespace python
+} // namespace boost
 
 #endif // REGISTER_PTR_TO_PYTHON_HPP
-
-
