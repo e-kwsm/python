33d29462954848cef7989f925a96e109b8d3866d
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 14:47:46 +0900

    include/boost/python/converter/pyobject_traits.hpp include/boost/python/converter/pyobject_type.hpp

--- include/boost/python/converter/pyobject_type.hpp
+++ include/boost/python/converter/pyobject_type.hpp
@@ -3,38 +3,35 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 #ifndef PYOBJECT_TYPE_DWA2002720_HPP
-# define PYOBJECT_TYPE_DWA2002720_HPP
+#define PYOBJECT_TYPE_DWA2002720_HPP
 
-# include <boost/python/cast.hpp>
+#include <boost/python/cast.hpp>
 
-namespace boost { namespace python { namespace converter { 
+namespace boost {
+namespace python {
+namespace converter {
 
-BOOST_PYTHON_DECL inline
-PyObject* checked_downcast_impl(PyObject *obj, PyTypeObject *type)
-{
+BOOST_PYTHON_DECL inline PyObject *checked_downcast_impl(PyObject *obj,
+                                                         PyTypeObject *type) {
   return (PyType_IsSubtype(Py_TYPE(obj), type) ? obj : NULL);
 }
 // Used as a base class for specializations which need to provide
 // Python type checking capability.
-template <class Object, PyTypeObject* pytype>
-struct pyobject_type 
-{
-    static bool check(PyObject* x)
-    {
-        return ::PyObject_IsInstance(x, (PyObject*)pytype);
-    }
+template <class Object, PyTypeObject *pytype> struct pyobject_type {
+  static bool check(PyObject *x) {
+    return ::PyObject_IsInstance(x, (PyObject *)pytype);
+  }
 
-    static Object* checked_downcast(PyObject* x)
-    {
-        return python::downcast<Object>(
-            (checked_downcast_impl)(x, pytype)
-            );
-    }
+  static Object *checked_downcast(PyObject *x) {
+    return python::downcast<Object>((checked_downcast_impl)(x, pytype));
+  }
 #ifndef BOOST_PYTHON_NO_PY_SIGNATURES
-    static PyTypeObject const* get_pytype() { return pytype; }
+  static PyTypeObject const *get_pytype() { return pytype; }
 #endif
 };
 
-}}} // namespace boost::python::converter
+} // namespace converter
+} // namespace python
+} // namespace boost
 
 #endif // PYOBJECT_TYPE_DWA2002720_HPP
