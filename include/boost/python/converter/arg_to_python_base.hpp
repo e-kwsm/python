96a18acd297eed7e62f813e39cb307e6b4897875
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 17:01:25 +0900

    include/boost/python/converter/arg_to_python_base.hpp include/boost/python/converter/as_to_python_function.hpp include/boost/python/converter/constructor_function.hpp include/boost/python/converter/context_result_converter.hpp include/boost/python/converter/convertible_function.hpp include/boost/python/converter/from_python.hpp include/boost/python/converter/implicit.hpp include/boost/python/converter/registered.hpp

--- include/boost/python/converter/arg_to_python_base.hpp
+++ include/boost/python/converter/arg_to_python_base.hpp
@@ -3,22 +3,23 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 #ifndef ARG_TO_PYTHON_BASE_DWA200237_HPP
-# define ARG_TO_PYTHON_BASE_DWA200237_HPP
-# include <boost/python/handle.hpp>
+#define ARG_TO_PYTHON_BASE_DWA200237_HPP
+#include <boost/python/handle.hpp>
 
-namespace boost { namespace python { namespace converter {
+namespace boost {
+namespace python {
+namespace converter {
 
 struct registration;
 
-namespace detail
-{
-  struct BOOST_PYTHON_DECL arg_to_python_base
-      : handle<>
-  {
-      arg_to_python_base(void const volatile* source, registration const&);
-  };
-}
+namespace detail {
+struct BOOST_PYTHON_DECL arg_to_python_base : handle<> {
+  arg_to_python_base(void const volatile *source, registration const &);
+};
+} // namespace detail
 
-}}} // namespace boost::python::converter
+} // namespace converter
+} // namespace python
+} // namespace boost
 
 #endif // ARG_TO_PYTHON_BASE_DWA200237_HPP
