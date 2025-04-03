fae4daebd9d15749b94a3427c036e3d2a10bca3d
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 18:07:03 +0900

    include/boost/python/make_constructor.hpp include/boost/python/make_function.hpp include/boost/python/manage_new_object.hpp include/boost/python/module.hpp

--- include/boost/python/module.hpp
+++ include/boost/python/module.hpp
@@ -3,15 +3,15 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 #ifndef MODULE_DWA2001128_HPP
-# define MODULE_DWA2001128_HPP
+#define MODULE_DWA2001128_HPP
 
-# include <boost/python/detail/prefix.hpp>
+#include <boost/python/detail/prefix.hpp>
 
-# include <boost/python/module_init.hpp>
-# define BOOST_PYTHON_MODULE BOOST_PYTHON_MODULE_INIT
-# if PY_VERSION_HEX >= 0x03050000
-#  define BOOST_PYTHON_MODULE_MULTI_PHASE BOOST_PYTHON_MODULE_MULTI_PHASE_INIT
-#  define BOOST_PYTHON_MODULE_WITH_STATE BOOST_PYTHON_MODULE_WITH_STATE_INIT
-# endif
+#include <boost/python/module_init.hpp>
+#define BOOST_PYTHON_MODULE BOOST_PYTHON_MODULE_INIT
+#if PY_VERSION_HEX >= 0x03050000
+#define BOOST_PYTHON_MODULE_MULTI_PHASE BOOST_PYTHON_MODULE_MULTI_PHASE_INIT
+#define BOOST_PYTHON_MODULE_WITH_STATE BOOST_PYTHON_MODULE_WITH_STATE_INIT
+#endif
 
 #endif // MODULE_DWA20011221_HPP
