1bc689c1b00a00e572521d9e1d9d0fee1dd3000e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 17:46:10 +0900

    include/boost/python/detail/map_entry.hpp include/boost/python/detail/mpl_lambda.hpp include/boost/python/detail/msvc_typeinfo.hpp

--- include/boost/python/detail/mpl_lambda.hpp
+++ include/boost/python/detail/mpl_lambda.hpp
@@ -3,10 +3,10 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 #ifndef MPL_LAMBDA_DWA2002122_HPP
-# define MPL_LAMBDA_DWA2002122_HPP
+#define MPL_LAMBDA_DWA2002122_HPP
 
 // this header should go away soon
-# include <boost/mpl/aux_/lambda_support.hpp>
-# define BOOST_PYTHON_MPL_LAMBDA_SUPPORT BOOST_MPL_AUX_LAMBDA_SUPPORT
+#include <boost/mpl/aux_/lambda_support.hpp>
+#define BOOST_PYTHON_MPL_LAMBDA_SUPPORT BOOST_MPL_AUX_LAMBDA_SUPPORT
 
 #endif // MPL_LAMBDA_DWA2002122_HPP
