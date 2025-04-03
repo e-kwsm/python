41abceb2a6e10514c696d34e7cc8809baedf29fb
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 16:58:27 +0900

    src/numpy/dtype.cpp src/numpy/matrix.cpp

--- src/numpy/matrix.cpp
+++ src/numpy/matrix.cpp
@@ -8,56 +8,51 @@
 #include <boost/python/numpy/internal.hpp>
 #include <boost/python/numpy/matrix.hpp>
 
-namespace boost { namespace python { namespace numpy
-{
-namespace detail 
-{
-inline object get_matrix_type()
-{
+namespace boost {
+namespace python {
+namespace numpy {
+namespace detail {
+inline object get_matrix_type() {
   object module = import("numpy");
   return module.attr("matrix");
 }
-} // namespace boost::python::numpy::detail
-} // namespace boost::python::numpy
+} // namespace detail
+} // namespace numpy
 
-namespace converter 
-{
+namespace converter {
 
-PyTypeObject const * object_manager_traits<numpy::matrix>::get_pytype()
-{
-  return reinterpret_cast<PyTypeObject*>(numpy::detail::get_matrix_type().ptr());
+PyTypeObject const *object_manager_traits<numpy::matrix>::get_pytype() {
+  return reinterpret_cast<PyTypeObject *>(
+      numpy::detail::get_matrix_type().ptr());
 }
 
-} // namespace boost::python::converter
+} // namespace converter
 
-namespace numpy 
-{
+namespace numpy {
 
-object matrix::construct(object const & obj, dtype const & dt, bool copy)
-{
+object matrix::construct(object const &obj, dtype const &dt, bool copy) {
   return numpy::detail::get_matrix_type()(obj, dt, copy);
 }
 
-object matrix::construct(object const & obj, bool copy)
-{
+object matrix::construct(object const &obj, bool copy) {
   return numpy::detail::get_matrix_type()(obj, object(), copy);
 }
 
-matrix matrix::view(dtype const & dt) const 
-{
-  return matrix(python::detail::new_reference
-    (PyObject_CallMethod(this->ptr(), const_cast<char*>("view"), const_cast<char*>("O"), dt.ptr())));
+matrix matrix::view(dtype const &dt) const {
+  return matrix(python::detail::new_reference(
+      PyObject_CallMethod(this->ptr(), const_cast<char *>("view"),
+                          const_cast<char *>("O"), dt.ptr())));
 }
 
-matrix matrix::copy() const 
-{
-  return matrix(python::detail::new_reference
-    (PyObject_CallMethod(this->ptr(), const_cast<char*>("copy"), const_cast<char*>(""))));
+matrix matrix::copy() const {
+  return matrix(python::detail::new_reference(PyObject_CallMethod(
+      this->ptr(), const_cast<char *>("copy"), const_cast<char *>(""))));
 }
 
-matrix matrix::transpose() const 
-{
+matrix matrix::transpose() const {
   return matrix(extract<matrix>(ndarray::transpose()));
 }
 
-}}} // namespace boost::python::numpy
+} // namespace numpy
+} // namespace python
+} // namespace boost
