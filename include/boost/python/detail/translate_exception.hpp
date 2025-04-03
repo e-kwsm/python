6560785dc31af472f641137c6c8a75b7388c764e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 17:56:00 +0900

    include/boost/python/detail/translate_exception.hpp include/boost/python/detail/type_list.hpp include/boost/python/detail/type_list_impl.hpp

--- include/boost/python/detail/translate_exception.hpp
+++ include/boost/python/detail/translate_exception.hpp
@@ -20,48 +20,42 @@ namespace detail {
 // ExceptionType into Python exceptions by invoking an object of type
 // Translate. Typically the translate function will be curried with
 // boost::bind().
-template <class ExceptionType, class Translate>
-struct translate_exception
-{
+template <class ExceptionType, class Translate> struct translate_exception {
 // workaround for broken gcc that ships with SuSE 9.0 and SuSE 9.1
-# if defined(__linux__) && defined(__GNUC__) \
-    && BOOST_WORKAROUND(__GNUC__, == 3) \
-    && BOOST_WORKAROUND(__GNUC_MINOR__, == 3) \
-    && (BOOST_WORKAROUND(__GNUC_PATCHLEVEL__, == 1) \
-        || BOOST_WORKAROUND(__GNUC_PATCHLEVEL__, == 3))
-    typedef typename remove_reference<
-        typename add_const<ExceptionType>::type
-    >::type exception_non_ref;
-# else
-    typedef typename add_lvalue_reference<
-        typename add_const<ExceptionType>::type
-    >::type exception_cref;
-# endif
-    
-    inline bool operator()(
-        exception_handler const& handler
-      , function0<void> const& f
-      , typename call_traits<Translate>::param_type translate) const
-    {
-        try
-        {
-            return handler(f);
-        }
-// workaround for broken gcc that ships with SuSE 9.0 and SuSE 9.1
-# if defined(__linux__) && defined(__GNUC__) \
-    && BOOST_WORKAROUND(__GNUC__, == 3) \
-    && BOOST_WORKAROUND(__GNUC_MINOR__, == 3) \
-    && (BOOST_WORKAROUND(__GNUC_PATCHLEVEL__, == 1) \
-        || BOOST_WORKAROUND(__GNUC_PATCHLEVEL__, == 3))
-        catch(exception_non_ref& e)
-# else
-        catch(exception_cref e)
-# endif
-        {
-            translate(e);
-            return true;
-        }
+#if defined(__linux__) && defined(__GNUC__) &&                                 \
+    BOOST_WORKAROUND(__GNUC__, == 3) &&                                        \
+    BOOST_WORKAROUND(__GNUC_MINOR__, == 3) &&                                  \
+    (BOOST_WORKAROUND(__GNUC_PATCHLEVEL__, == 1) ||                            \
+     BOOST_WORKAROUND(__GNUC_PATCHLEVEL__, == 3))
+  typedef
+      typename remove_reference<typename add_const<ExceptionType>::type>::type
+          exception_non_ref;
+#else
+  typedef typename add_lvalue_reference<
+      typename add_const<ExceptionType>::type>::type exception_cref;
+#endif
+
+  inline bool
+  operator()(exception_handler const &handler, function0<void> const &f,
+             typename call_traits<Translate>::param_type translate) const {
+    try {
+      return handler(f);
     }
+// workaround for broken gcc that ships with SuSE 9.0 and SuSE 9.1
+#if defined(__linux__) && defined(__GNUC__) &&                                 \
+    BOOST_WORKAROUND(__GNUC__, == 3) &&                                        \
+    BOOST_WORKAROUND(__GNUC_MINOR__, == 3) &&                                  \
+    (BOOST_WORKAROUND(__GNUC_PATCHLEVEL__, == 1) ||                            \
+     BOOST_WORKAROUND(__GNUC_PATCHLEVEL__, == 3))
+    catch (exception_non_ref &e)
+#else
+    catch (exception_cref e)
+#endif
+    {
+      translate(e);
+      return true;
+    }
+  }
 };
 
 } // namespace detail
