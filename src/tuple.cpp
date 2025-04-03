b399315f003e0092e5ebd68f486bd143d5fe3526
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 16:36:28 +0900

    src/str.cpp src/tuple.cpp

--- src/tuple.cpp
+++ src/tuple.cpp
@@ -4,32 +4,28 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 #include <boost/python/tuple.hpp>
 
-namespace boost { namespace python { namespace detail {
+namespace boost {
+namespace python {
+namespace detail {
 
-detail::new_reference tuple_base::call(object const& arg_)
-{
-    return (detail::new_reference)PyObject_CallFunction(
-        (PyObject*)&PyTuple_Type, const_cast<char*>("(O)"), 
-        arg_.ptr());
+detail::new_reference tuple_base::call(object const &arg_) {
+  return (detail::new_reference)PyObject_CallFunction(
+      (PyObject *)&PyTuple_Type, const_cast<char *>("(O)"), arg_.ptr());
 }
-    
-tuple_base::tuple_base()
-    : object(detail::new_reference(PyTuple_New(0)))
-{}
-    
-tuple_base::tuple_base(object_cref sequence)
-    : object(call(sequence))
-{}
 
-static struct register_tuple_pytype_ptr
-{
-    register_tuple_pytype_ptr()
-    {
-        const_cast<converter::registration &>(
-            converter::registry::lookup(boost::python::type_id<boost::python::tuple>())
-            ).m_class_object = &PyTuple_Type;
-    }
-}register_tuple_pytype_ptr_;
+tuple_base::tuple_base() : object(detail::new_reference(PyTuple_New(0))) {}
 
+tuple_base::tuple_base(object_cref sequence) : object(call(sequence)) {}
 
-}}}  // namespace boost::python
+static struct register_tuple_pytype_ptr {
+  register_tuple_pytype_ptr() {
+    const_cast<converter::registration &>(
+        converter::registry::lookup(
+            boost::python::type_id<boost::python::tuple>()))
+        .m_class_object = &PyTuple_Type;
+  }
+} register_tuple_pytype_ptr_;
+
+} // namespace detail
+} // namespace python
+} // namespace boost
