dd48a547e18e8d0c041765c4a32835b6ac091eb8
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 16:25:17 +0900

    test/a_map_indexing_suite.cpp test/aligned_class.cpp test/andreas_beyer.cpp

--- test/aligned_class.cpp
+++ test/aligned_class.cpp
@@ -10,24 +10,21 @@
 
 using namespace boost::python;
 
-struct BOOST_ALIGNMENT(32) X
-{
-    int x;
-    BOOST_ALIGNMENT(32) float f;
-    X(int n, float _f) : x(n), f(_f)
-    {
-        BOOST_ASSERT((reinterpret_cast<uintptr_t>(&f) % 32) == 0);
-    }
+struct BOOST_ALIGNMENT(32) X {
+  int x;
+  BOOST_ALIGNMENT(32) float f;
+  X(int n, float _f) : x(n), f(_f) {
+    BOOST_ASSERT((reinterpret_cast<uintptr_t>(&f) % 32) == 0);
+  }
 };
 
-int x_function(X& x) { return x.x;}
-float f_function(X& x) { return x.f;}
+int x_function(X &x) { return x.x; }
+float f_function(X &x) { return x.f; }
 
-BOOST_PYTHON_MODULE(aligned_class_ext)
-{
-    class_<X>("X", init<int, float>());
-    def("x_function", x_function);
-    def("f_function", f_function);
+BOOST_PYTHON_MODULE(aligned_class_ext) {
+  class_<X>("X", init<int, float>());
+  def("x_function", x_function);
+  def("f_function", f_function);
 }
 
 #include "module_tail.cpp"
