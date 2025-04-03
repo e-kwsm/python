007262ebcb00c870124e8d53b0061ff9a46dbb12
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 16:35:03 +0900

    src/exec.cpp src/import.cpp src/list.cpp src/long.cpp

--- src/import.cpp
+++ src/import.cpp
@@ -8,18 +8,15 @@
 #include <boost/python/extract.hpp>
 #include <boost/python/handle.hpp>
 
-namespace boost 
-{ 
-namespace python 
-{
+namespace boost {
+namespace python {
 
-object BOOST_PYTHON_DECL import(str name)
-{
+object BOOST_PYTHON_DECL import(str name) {
   // should be 'char const *' but older python versions don't use 'const' yet.
   char *n = python::extract<char *>(name);
   python::handle<> module(PyImport_ImportModule(n));
   return python::object(module);
 }
 
-}  // namespace boost::python
-}  // namespace boost
+} // namespace python
+} // namespace boost
