55d509ec6ad802b1919b293e79e736562de46066
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-03 18:40:01 +0900

    include/boost/python/numpy/numpy_object_mgr_traits.hpp include/boost/python/numpy/scalars.hpp include/boost/python/numpy/ufunc.hpp

--- include/boost/python/numpy/numpy_object_mgr_traits.hpp
+++ include/boost/python/numpy/numpy_object_mgr_traits.hpp
@@ -10,29 +10,25 @@
 #include <boost/python/numpy/config.hpp>
 
 /**
- *  @brief Macro that specializes object_manager_traits by requiring a 
+ *  @brief Macro that specializes object_manager_traits by requiring a
  *         source-file implementation of get_pytype().
  */
 
-#define NUMPY_OBJECT_MANAGER_TRAITS(manager)                            \
-template <>								\
-struct BOOST_NUMPY_DECL object_manager_traits<manager>			\
-{									\
-  BOOST_STATIC_CONSTANT(bool, is_specialized = true);			\
-  static inline python::detail::new_reference adopt(PyObject* x)	\
-  {									\
-    return python::detail::new_reference(python::pytype_check((PyTypeObject*)get_pytype(), x)); \
-  }									\
-  static bool check(PyObject* x)					\
-  {									\
-    return ::PyObject_IsInstance(x, (PyObject*)get_pytype());		\
-  }									\
-  static manager* checked_downcast(PyObject* x)				\
-  {									\
-    return python::downcast<manager>((checked_downcast_impl)(x, (PyTypeObject*)get_pytype())); \
-  }									\
-  static PyTypeObject const * get_pytype();				\
-}
+#define NUMPY_OBJECT_MANAGER_TRAITS(manager)                                   \
+  template <> struct BOOST_NUMPY_DECL object_manager_traits<manager> {         \
+    BOOST_STATIC_CONSTANT(bool, is_specialized = true);                        \
+    static inline python::detail::new_reference adopt(PyObject *x) {           \
+      return python::detail::new_reference(                                    \
+          python::pytype_check((PyTypeObject *)get_pytype(), x));              \
+    }                                                                          \
+    static bool check(PyObject *x) {                                           \
+      return ::PyObject_IsInstance(x, (PyObject *)get_pytype());               \
+    }                                                                          \
+    static manager *checked_downcast(PyObject *x) {                            \
+      return python::downcast<manager>(                                        \
+          (checked_downcast_impl)(x, (PyTypeObject *)get_pytype()));           \
+    }                                                                          \
+    static PyTypeObject const *get_pytype();                                   \
+  }
 
 #endif
-
