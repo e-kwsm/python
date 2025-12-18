4f4746214cd22d49433c41ded378134160ba7979
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-12-18 22:08:37 +0900

    src/wrapper.cpp

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -40,24 +40,19 @@ override wrapper_base::get_override(char const *name,
 } // namespace detail
 
 #if 0
-namespace converter
-{
-  PyObject* BOOST_PYTHON_DECL do_polymorphic_ref_to_python(
-      python::detail::wrapper_base const volatile* x, type_info src
-  )
-  {
-      if (x == 0)
-      {
-          ::PyErr_Format(
-              PyExc_TypeError
-            , "Attempting to returning pointer or reference to instance of %s\n"
-              "for which no corresponding Python object exists. Wrap this function"
-              "with a return return value policy"
-          )
-      }
+namespace converter {
+PyObject *BOOST_PYTHON_DECL do_polymorphic_ref_to_python(
+    python::detail::wrapper_base const volatile *x, type_info src) {
+  if (x == 0) {
+    ::PyErr_Format(
+        PyExc_TypeError,
+        "Attempting to returning pointer or reference to instance of %s\n"
+        "for which no corresponding Python object exists. Wrap this function"
+        "with a return return value policy")
   }
-  
 }
+
+} // namespace converter
 #endif
 
 } // namespace python
