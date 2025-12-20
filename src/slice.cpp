76924c29c5d8320ef3d9b6b760a869c27f7fa66f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-12-20 16:42:45 +0900

    src/slice.cpp

--- src/slice.cpp
+++ src/slice.cpp
@@ -9,29 +9,29 @@
 namespace boost { namespace python { namespace detail {
 
 slice_base::slice_base(PyObject* start, PyObject* stop, PyObject* step)
-  : object(detail::new_reference( PySlice_New(start, stop, step)))
+  : object(reinterpret_cast<detail::new_reference>(PySlice_New(start, stop, step)))
 {
 }
 
 object
 slice_base::start() const
 {
-    return object( detail::borrowed_reference(
-        ((PySliceObject*)this->ptr())->start));
+    return object(reinterpret_cast<detail::borrowed_reference>(
+        (reinterpret_cast<PySliceObject*>(this->ptr()))->start));
 }
 
 object
 slice_base::stop() const
 {
-    return object( detail::borrowed_reference(
-        ((PySliceObject*)this->ptr())->stop));
+    return object(reinterpret_cast<detail::borrowed_reference>(
+        (reinterpret_cast<PySliceObject*>(this->ptr()))->stop));
 }
 
 object
 slice_base::step() const
 {
-    return object( detail::borrowed_reference(
-        ((PySliceObject*)this->ptr())->step));
+    return object(reinterpret_cast<detail::borrowed_reference>(
+        (reinterpret_cast<PySliceObject*>(this->ptr()))->step));
 }
 
 static struct register_slice_pytype_ptr
