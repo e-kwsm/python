d81e46ee5b9812097f53b8ec4a07b759e47c0d12
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-05 15:27:53 +0900

    revert

--- include/boost/python/suite/indexing/indexing_suite.hpp
+++ include/boost/python/suite/indexing/indexing_suite.hpp
@@ -188,7 +188,6 @@ namespace boost { namespace python {
                 .def("clear", &base_clear)
                 // .def("insert", ())
                 .def("pop", &base_pop)
-                .def("count", &base_count)
             ;
 
             DerivedPolicies::extension_def(cl);
@@ -311,18 +310,6 @@ namespace boost { namespace python {
             }
                     PyErr_SetString(PyExc_IndexError, "pop index out of range");
                     throw_error_already_set();
-        }
-
-        static size_t
-        base_count(Container& container, PyObject* key)
-        {
-            extract<const Key&> x(key);
-            if (x.check()) {
-                return DerivedPolicies::count(container, x());
-            } else {
-                extract<Key> x(key);
-                return x.check() ? DerivedPolicies::contains(container, x()) : 0;
-            }
         }
     };
 
