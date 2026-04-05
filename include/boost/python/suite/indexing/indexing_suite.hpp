a61427a833e9c61a2981852be54a5eb1891d8fd1
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-05 16:00:58 +0900

    Revert "revert"
    This reverts commit 5242458925c0c4d6a3c6b53ed18273da2fe9c25b.

--- include/boost/python/suite/indexing/indexing_suite.hpp
+++ include/boost/python/suite/indexing/indexing_suite.hpp
@@ -188,6 +188,7 @@ namespace boost { namespace python {
                 .def("clear", &base_clear)
                 // .def("insert", ())
                 .def("pop", &base_pop)
+                .def("count", &base_count)
             ;
 
             DerivedPolicies::extension_def(cl);
@@ -310,6 +311,18 @@ namespace boost { namespace python {
             }
                     PyErr_SetString(PyExc_IndexError, "pop index out of range");
                     throw_error_already_set();
+        }
+
+        static size_t
+        base_count(Container& container, PyObject* key)
+        {
+            extract<const Key&> x(key);
+            if (x.check()) {
+                return DerivedPolicies::count(container, x());
+            } else {
+                extract<Key> x(key);
+                return x.check() ? DerivedPolicies::contains(container, x()) : 0;
+            }
         }
     };
 
