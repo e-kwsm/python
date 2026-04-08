10aa06961fcdf473aae74f5e0e58dde219893919
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-08 10:43:53 +0900

    test/callbacks.cpp test/test_class.hpp

--- test/test_class.hpp
+++ test/test_class.hpp
@@ -12,6 +12,7 @@ struct test_class
     explicit test_class(int x) : x(x), magic(7654321 + n) { ++counter; }
     test_class(test_class const& rhs) : x(rhs.x), magic(7654321 + n) { ++counter; }
     virtual ~test_class() { BOOST_ASSERT(magic == 7654321 + n); magic = 6666666; x = 9999; --counter; }
+    void operator=(test_class const&) = delete;
 
     void set(int _x) { BOOST_ASSERT(magic == 7654321 + n); this->x = _x; }
     int value() const { BOOST_ASSERT(magic == 7654321 + n); return x; }
@@ -21,9 +22,6 @@ struct test_class
     int x;
     long magic;
     static int counter;
-    
- private:
-    void operator=(test_class const&);
 };
 
 template <int n>
